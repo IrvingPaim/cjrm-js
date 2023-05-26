eaba932cb963a2812419774fee040ed5ce627de6
Irving Paim <irvingpaim@yahoo.com.br>
2023-05-26 19:42:57 -0300

    primeira parte app weather

--- weather-application-master/js/weather.js
+++ weather-application-master/js/weather.js
@@ -1,2 +1,24 @@
 const APIKey = '9bHlPGHfSFblFhYtdoSdS0ZP49DAamdx'
+const getCityUrl = cityName => 
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+    try {
+        const cityUrl = getCityUrl(cityName)
+        const response = await fetch(cityUrl)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados')
+        }
+
+        const [cityData] = await response.json()
+        return cityData
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityData('salvador')
+
+
 
