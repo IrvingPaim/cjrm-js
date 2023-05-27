0e66e74a654a44b4513be21e5290e665c4b6c413
Irving Paim <irvingpaim@yahoo.com.br>
2023-05-26 21:49:45 -0300

    2 parte weather exercicio

--- weather-application-master/js/weather.js
+++ weather-application-master/js/weather.js
@@ -20,5 +20,24 @@ const getCityData = async cityName => {
 
 getCityData('salvador')
 
+const getCityWeather = async cityName => {
+    try {
+        const { Key } = await getCityData(cityName)
+        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
+        const response = await fetch(cityWeatherUrl)
+
+        if (!response.ok) {
+            throw new Error('Não foi possível obter os dados')
+        }
+
+        const [cityWeatherData] = await response.json()
+        console.log(cityWeatherData)
+    } catch ({ name, message }) {
+        alert(`${name}: ${message}`)
+    }
+}
+
+getCityWeather('salvador')
+
 
 
