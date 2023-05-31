13baadf6c4f2cc4929547d03654cff2ac847a4d3
Irving Paim <irvingpaim@yahoo.com.br>
2023-05-30 21:48:03 -0300

    Atualização do exercício 33 app weather

--- weather-application-master/js/weather.js
+++ weather-application-master/js/weather.js
@@ -1,43 +1,44 @@
 const APIKey = '9bHlPGHfSFblFhYtdoSdS0ZP49DAamdx'
-const getCityUrl = cityName => 
-    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+const baseUrl = 'http://dataservice.accuweather.com/'
 
-const getCityData = async cityName => {
+const getCityUrl = cityName => 
+    `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+// Função que retorna a URL necessária para a realização do request sobre os dados da cidade
+
+const getWeatherUrl = ({ Key }) => 
+    `${baseUrl}currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+
+// Função que retorna a URL necessária para a realização de um 2º request com os dados sobre o clima da cidade (1º request)
+
+const fetchData = async url => {
     try {
-        const cityUrl = getCityUrl(cityName)
-        const response = await fetch(cityUrl)
+        const response = await fetch(url)
 
         if (!response.ok) {
             throw new Error('Não foi possível obter os dados')
         }
 
-        const [cityData] = await response.json()
-        return cityData
+        return response.json()
     } catch ({ name, message }) {
         alert(`${name}: ${message}`)
     }
 }
 
-getCityData('salvador')
+// Função genéria que realiza o request e retorna uma PROMISE com os dados da requisição
+
+const getCityData = cityName => fetchData(getCityUrl(cityName))
+
+// Função que retorna uma PROMISE com um array de objetos de cidades
 
 const getCityWeather = async cityName => {
-    try {
-        const { Key } = await getCityData(cityName)
-        const cityWeatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${Key}?apikey=${APIKey}`
-        const response = await fetch(cityWeatherUrl)
-
-        if (!response.ok) {
-            throw new Error('Não foi possível obter os dados')
-        }
-
-        const [cityWeatherData] = await response.json()
-        console.log(cityWeatherData)
-    } catch ({ name, message }) {
-        alert(`${name}: ${message}`)
-    }
+    const [cityData] = await getCityData(cityName)
+    return fetchData(getWeatherUrl(cityData))
 }
 
-getCityWeather('salvador')
+// Função que retorna uma PROMISE com as condições climáticas da cidade no index 0 do array de objetos
 
+getCityWeather('Salvador').then(console.log)
 
+// Invocação da função que retorna um array com o objeto contendo as condições climáticas da cidade passada por parâmetro
 
