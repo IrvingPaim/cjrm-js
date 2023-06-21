db73886bb798008a2734b1f36f979004d5a1e7bd
Irving Paim <irvingpaim@yahoo.com.br>
2023-06-20 21:19:32 -0300

    Finalização

--- exercicio-36/app.js
+++ exercicio-36/app.js
@@ -192,3 +192,31 @@ console.log(accumulator)
     3 No passo 3.4, se o slide exibido atualmente não corresponder ao index do 
       1º slide, o slide anterior deve ser exibido.
 */
+
+const slides = document.querySelectorAll('[data-js="carousel__item"]')
+const nextButton = document.querySelector('[data-js="carousel__button--next"]')
+const prevButton = document.querySelector('[data-js="carousel__button--prev"]')
+
+const lastSlideIndex = slides.length - 1
+let currentSlideIndex = 0
+
+const manipulatedSlidesClasses = correctSlideIndex => {
+  slides.forEach(slide => slide.classList.remove('carousel__item--visible'))
+  slides[correctSlideIndex].classList.add('carousel__item--visible')
+}
+
+nextButton.addEventListener('click', () => {
+  const correctSlideIndex = currentSlideIndex === lastSlideIndex
+    ? currentSlideIndex = 0
+    : ++currentSlideIndex
+
+    manipulatedSlidesClasses(correctSlideIndex) 
+})
+
+prevButton.addEventListener('click', () => {
+  const correctSlideIndex = currentSlideIndex === 0 
+    ? currentSlideIndex = lastSlideIndex 
+    : --currentSlideIndex
+
+  manipulatedSlidesClasses(correctSlideIndex)
+})
