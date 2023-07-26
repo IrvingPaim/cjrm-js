6292ef690a5fa2fbadf72f9b36be18790d83eaa7
Irving Paim <irvingpaim@yahoo.com.br>
2023-07-25 21:15:57 -0300

    Finalizada a segunda parte do exercício 39

--- exercicio-39/app.js
+++ exercicio-39/app.js
@@ -49,16 +49,38 @@ console.log(w, y, z)
 
 const accordion = document.querySelector('[data-js="accordion"]')
 
-accordion.addEventListener('click', e => {
+const closeAccordionItem = accordionHeaderToBeClosed => {
+  const accordionHeaderId = accordionHeaderToBeClosed.dataset.accordionHeader
+  const accordionBodyToBeClosed = 
+    document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+  
+  accordionHeaderToBeClosed.classList.remove('active')
+  accordionBodyToBeClosed.classList.remove('active')
+}
+
+const handleAccordionClick = e => {
   const accordionHeaderId = e.target.dataset.accordionHeader
   const clickedAccordionHeader = 
     document.querySelector(`[data-accordion-header="${accordionHeaderId}"]`)
   const accordionItemToBeOpened = 
     document.querySelector(`[data-accordion-body="${accordionHeaderId}"]`)
+  const accordionHeaderToBeClosed = Array
+    .from(document.querySelectorAll('[data-js="accordion-header"]'))
+    .filter(accordionHeader => accordionHeader !== clickedAccordionHeader)
+    .find(accordionHeader => accordionHeader.classList.contains('active'))
 
-    clickedAccordionHeader.classList.toggle('active')
-    accordionItemToBeOpened.classList.toggle('active')
-})
+  if (!e.target.dataset.accordionHeader) {
+    return
+  }
+
+  if (accordionHeaderToBeClosed) {
+    closeAccordionItem(accordionHeaderToBeClosed)
+  }
+  clickedAccordionHeader.classList.toggle('active')
+  accordionItemToBeOpened.classList.toggle('active')
+}
+
+accordion.addEventListener('click', handleAccordionClick)
 
 /*
   03
